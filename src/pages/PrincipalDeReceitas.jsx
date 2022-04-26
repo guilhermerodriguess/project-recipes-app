bffaa86da1325451511401687f9ace6ebbf6a239
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-04-26 16:45:23 -0300

    Cumprimento do requisito 25

--- src/pages/PrincipalDeReceitas.jsx
+++ src/pages/PrincipalDeReceitas.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Header from '../components/Header';
 
-const PrincipalDeReceitas = () => (
-  <div>
-    <p>Tela Principal de Receitas</p>
-  </div>
-);
+const PrincipalDeReceitas = () => {
+  const foods = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+  return (
+    <section>
+      <Header />
+      {
+        foods.map((food, index) => (
+          <div
+            key={ index }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <img
+              src=""
+              alt="Food-ilustration"
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{ food }</p>
+          </div>
+        ))
+      }
+    </section>
+  );
+};
 
 export default PrincipalDeReceitas;
