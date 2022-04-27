f53d75fc722a99ee7e36c8eb9aa34d58fc6bba53
Guilherme R <grsguiga@gmail.com>
2022-04-27 10:17:12 -0300

    Adiciona pasta de testes

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -5,9 +5,11 @@ import RecipeContext from '../context/RecipesContext';
 const CardRecipe = () => {
   const { data } = useContext(RecipeContext);
   const MAX_RECIPES = 12;
+  // Filtra os 12 primeiros resultados.
   const maxRecipes = data.filter((element, index) => index < MAX_RECIPES);
   const history = useHistory();
 
+  // Caso o Card esteja em /foods, renderiza as chaves de Foods.
   if (history.location.pathname === '/foods') {
     return maxRecipes.map(({ strMeal, strMealThumb }, index) => (
       <div key={ Math.random() } data-testid={ `${index}-recipe-card` }>
@@ -24,6 +26,8 @@ const CardRecipe = () => {
       </div>
     ));
   }
+
+  // Caso o Card esteja em /drinks, renderiza as chaves de Drinks.
   if (history.location.pathname === '/drinks') {
     return maxRecipes.map(({ strDrink, strDrinkThumb }, index) => (
       <div key={ Math.random() } data-testid={ `${index}-recipe-card` }>
