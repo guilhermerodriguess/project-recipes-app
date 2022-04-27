d29b349e0d6c4f26d8ac7d91760df5dddfc17203
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-04-27 14:26:41 -0300

    Renderização dos cards

--- src/pages/PrincipalDeReceitas.jsx
+++ src/pages/PrincipalDeReceitas.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import Header from '../components/Header';
+import MenuInferior from '../components/MenuInferior';
 
 const PrincipalDeReceitas = () => {
   const foods = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
   return (
-    <section>
+    <>
       <Header />
-      {
-        foods.map((food, index) => (
-          <div
-            key={ index }
-            data-testid={ `${index}-recipe-card` }
-          >
-            <img
-              src=""
-              alt="Food-ilustration"
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{ food }</p>
-          </div>
-        ))
-      }
-    </section>
+      <section>
+        {
+          foods.map((food, index) => (
+            <div
+              key={ `${index}-recipe-card` }
+              data-testid={ `${index}-recipe-card` }
+            >
+              <img
+                src=""
+                alt="Food-ilustration"
+                data-testid={ `${index}-card-img` }
+              />
+              <p data-testid={ `${index}-card-name` }>{ food }</p>
+            </div>
+          ))
+        }
+      </section>
+      <MenuInferior />
+    </>
   );
 };
 
